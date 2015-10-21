09cab384146b9fc9e51282bf8671edc1568a91f0
Joel Nieman <niemanjoel@gmail.com>
2015-10-21 16:52:15 -0400

    added 2 category picker to initial view controller git status :q q clear

--- RestaurantReviews/PickerViewController.swift
+++ RestaurantReviews/PickerViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class PickerViewController: UIViewController {
+class PickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
 
+    @IBOutlet weak var categoryPicker: UIPickerView!
+    private let categoryComponent = 0
+    private let priceComponent = 1
+    private let categories = ["Breakfast", "Lunch", "Dinner", "Late Night"]
+    private let prices = ["Cheap", "Average", "Expensive"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+
+    @IBAction func seeResultsButtonPressed(sender: AnyObject) {
+
+        let categoryRow = categoryPicker.selectedRowInComponent(0)
+        let priceRow = categoryPicker.selectedRowInComponent(1)
+        
+        let categorySelected = categories[categoryRow]
+        let priceSelected = prices[priceRow]
+        
+        // the row and categorySelected constants will be used to call restaurants from the yelp API.
+
+        // ERIKA's SUGGESTION IS TO REPLACE THIS FUNCTION WITH A PREPARE FOR SEGUE FUNCTION AND INCLUDE THE SAME CODES + PROVIDE THE VALUE FOR THE API CALL TO YELP.
+        
+    }
+
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView:UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == categoryComponent {
+            return categories.count
+        } else {
+            return prices.count
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == categoryComponent {
+            return categories[row]
+        } else {
+            return prices[row]
+        }
+    }
+    
+    @IBAction override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        <#code#>
     }
 }
